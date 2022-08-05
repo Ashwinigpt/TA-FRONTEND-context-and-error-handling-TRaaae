aff36a2f38b8031fdfe84d607fe309bfb90916e5
Ashwini Gupta <ashwinigpt1996@gmail.com>
2022-08-05 11:36:32 +0530

    Done error boundries

--- block-BRaadi/code/src/components/App.js
+++ block-BRaadi/code/src/components/App.js
@@ -3,6 +3,10 @@ import Header from "./Header";
 import Main from "./Main";
 
 class App extends React.Component {
+  state = {
+    isDarkMode: false,
+  };
+  
   render() {
     let { isDarkMode } = this.state;
     return (
